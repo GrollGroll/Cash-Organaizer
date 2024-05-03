abcc6225145aa12a6e596c6a070045195ba71d1e
Anton <149895339+GrollGroll@users.noreply.github.com>
2024-05-03 23:08:04 +0300

    Delete exp.py

--- exp.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-date_str = input('Введите дату (ГГГГ-ММ-ДД): ')
-date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
-
-print(date_obj)
-print(type(date_obj))
